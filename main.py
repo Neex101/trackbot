e7ad8474fd1c1f936c09bce188d634245c3c0e74
Neex101 <skinnyed@gmail.com>
2023-02-28 17:55:47 +0000

    typo im/img

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ picam2.configure(preview_config)
 picam2.start()
 
 while True:
-    im = picam2.capture_array()
+    img = picam2.capture_array()
 
-    grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(grey, 1.1, 5)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0))
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0))
         cx=int(x+x+w)//2
         cy=int(y+y+h)//2
         cv2.circle(img,(cx,cy),5,(0,0,255),-1)
 
-    cv2.imshow("Camera", im)
+    cv2.imshow("Camera", img)

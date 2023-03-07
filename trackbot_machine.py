325db475edd46e9c91bfea8abd55698d6d11cb4e
Neex101 <skinnyed@gmail.com>
2023-03-07 23:59:46 +0000

    Initial

---
+++ trackbot_machine.py
@@ -0,0 +1,34 @@
+'''
+Created on 7th March 2023
+@author: Ed
+This module defines the machine's properties (e.g. travel), services (e.g. serial comms) and functions (e.g. move left)
+'''
+
+import serial_connection
+from kivy.clock import Clock
+import sys, os, time
+from datetime import datetime
+import os.path
+from os import path
+
+#from __builtin__ import True
+
+
+def log(message):
+    timestamp = datetime.now()
+    print (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + str(message))
+
+
+class TrackBotMachine(object):
+
+# SETUP
+    
+    s = None # serial object
+
+    def __init__(self, screen_manager):
+
+        self.sm = screen_manager
+
+        # Establish 's'erial comms and initialise
+        self.s = serial_connection.SerialConnection(self, self.sm, self.sett, self.l, self.jd)
+        self.s.autodetect_port_and_establish_connection()

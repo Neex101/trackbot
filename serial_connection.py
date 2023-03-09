57cf21bc5f543bf6879a3170de619f963f8bca69
Neex101 <skinnyed@gmail.com>
2023-03-09 21:04:04 +0000

    tidy

--- serial_connection.py
+++ serial_connection.py
@@ -17,12 +17,6 @@ from functools import partial
 from serial.serialutil import SerialException
 
 
-
-BAUD_RATE = 115200
-ENABLE_STATUS_REPORTS = True
-GRBL_SCANNER_MIN_DELAY = 0.01 # Delay between checking for response from grbl. Needs to be hi-freq for quick streaming, e.g. 0.01 = 100Hz
-
-
 def log(message):
     timestamp = datetime.now()
     print (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + str(message))
@@ -30,10 +24,11 @@ def log(message):
 
 class SerialConnection(object):
 
-    STATUS_INTERVAL = 0.1 # How often to poll general status to update UI (0.04 = 25Hz = smooth animation)
-
     s = None    # Serial comms object
     sm = None   # Screen manager object
+    
+    # Serial connection details
+    BAUD_RATE = 115200 
 
     def __init__(self, machine, screen_manager):
         self.m = machine
@@ -43,24 +38,10 @@ class SerialConnection(object):
         if self.s: self.s.close()
         log('Serial connection destructor')
 
-    # Unused utility function to bypass autodetect port function for debug
-    def quick_connect(self, available_port):
-        try: 
-            log("Try to connect to: " + available_port)
-            # set up connection
-            self.s = serial.Serial(str(available_port), BAUD_RATE, timeout = 6, writeTimeout = 20) # assign
-            self.s.flushInput()
-            self.s.write("\x18")
-            return available_port
-        
-        except:
-            log("Could not connect to given port.")
-            return ''
+    # Primary fuunction to establish serial connection
+    def detect_ports_and_establish_serial_connection(self):
 
-    # Primary fuunction  to establish connection
-    def autodetect_port_and_establish_connection(self):
-
-        log('Start to establish connection...')
+        log('Attempt serial connection...')
         trackbot_port = ''
 
         # if Windows, for dev
@@ -73,13 +54,9 @@ class SerialConnection(object):
             log("Windows port list: " + str(port_list)) # for debugging
 
             for comport in port_list:
-
-                log("Trying Windows port: " + comport)            
+                log("Trying port: " + comport)            
                 trackbot_port = self.is_port_actually_a_trackbot(comport)
                 if trackbot_port: break
-                
-            if not trackbot_port: 
-                log("No trackbot connected")
 
         # If Linux
         else:
@@ -96,18 +73,20 @@ class SerialConnection(object):
 
             # set up serial connection with first (most preferred) available port
             for available_port in list_of_available_ports:
+                log("Trying port: " + comport)            
                 trackbot_port = self.is_port_actually_a_trackbot('/dev/' + str(available_port))
                 if trackbot_port: break
 
         # Display result         
         if self.is_connected():
-            log("Serial connection made, on port:" + str(trackbot_port)) + ". It's go time."
+            log("Serial connection made to TrackBot, on port:" + str(trackbot_port)) + ". It's go time."
         else:
             log("Serial connection not made.")
 
 
     def is_port_actually_a_trackbot(self, available_port):
 
+        # Expected send/receive data
         ping_message = b"M115\n"
         expected_response_key = "FIRMWARE_NAME:Marlin"
         
@@ -116,19 +95,18 @@ class SerialConnection(object):
             self.s = serial.Serial(str(available_port), BAUD_RATE, timeout = 1, writeTimeout = 1) # assign
 
             try:
-
                 self.s.flushInput()
                 log("Pinging...")
                 self.s.write(ping_message)
 
                 first_line = self.s.readline()
-                log("Is port Trackbot? " + str(available_port) + " | First read: " + str(first_line))
+                log("Pinged:" + str(available_port) + " | Response: " + str(first_line))
 
                 if first_line:
 
                     if expected_response_key in str(first_line):
 
-                        log("Expected response: FOUND TRACKBOT!")
+                        log("Expected response :-)")
                         self.s.flushInput()
                         return available_port
 
@@ -158,18 +136,3 @@ class SerialConnection(object):
         else: 
             return False
 
-    # called by first kivy screen when safe to assume kivy processing is completed, to ensure correct clock scheduling
-    def start_services(self, dt):
-
-        log('Starting services')
-        self.s.flushInput()  # Flush startup text in serial input
-        self.next_poll_time = time.time()
-        self.grbl_scanner_running = True
-        t = threading.Thread(target=self.grbl_scanner)
-        t.daemon = True
-        t.start()
-        
-        # Clear any hard switch presses that may have happened during boot
-        self.m.bootup_sequence()
-
-        self.m.starting_serial_connection = False

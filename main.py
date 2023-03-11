389c0094a3f7aab0283e8f6425e52546307326ee
Neex101 <skinnyed@gmail.com>
2023-03-11 19:37:39 +0000

    serial connection logic win/linux

--- main.py
+++ main.py
@@ -40,13 +40,18 @@ class TrackBotUI(App):
 
     def build(self):
 
+        is_make_serial_connection = None
+
         log("Starting App.")
 
         # Screens manager object. This will be passed to objects so that they can influence the screens.
         sm = ScreenManager(transition=NoTransition())
 
         # Physical representation of the robot. It contains movement commands, and it's own camera and serial objects. 
-        m = trackbot_machine.TrackBotMachine(sm, False)
+        if sys.platform == "win32": is_make_serial_connection = False
+        else: is_make_serial_connection = True
+            
+        m = trackbot_machine.TrackBotMachine(sm, is_make_serial_connection)
 
         # The pilot is the control mechanism which reads the inputs and tells the machine what to do - so it's the algorithm of how things move.
         p = pilot.Pilot(sm, m)

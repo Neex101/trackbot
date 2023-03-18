c4165d52985663fec1716964b62124e3177d9d93
Neex101 <skinnyed@gmail.com>
2023-03-18 20:02:34 +0000

    temp

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ class TrackBotUI(App):
         # should serial connection be made? Saves time in dev to not make connection sometimes.
         is_make_serial_connection = None
         if sys.platform == "win32": is_make_serial_connection = False # my dev environment is Windows
-        else: is_make_serial_connection = True # TrackBot's OS is Linux
+        else: is_make_serial_connection = False # TrackBot's OS is Linux
 
         log("Starting App.")
 
